0520df8c87ebfd1519cf1640a43112884a4eae0f
Ralph - Collage Creative <ralphr@outlook.com>
2020-08-08 17:02:01 +0100

    Typo in docstring 4x", should be 3x

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    """ View to display homepage """"
+    """ View to display homepage """
     return render(request, 'homepage/index.html')
