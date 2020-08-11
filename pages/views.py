cb7db7364af413e86a18cefaebdb7d6b1bb963fd
redlik <ralphr@outlook.com>
2020-08-11 12:04:49 +0100

    2 views created to show pages templates

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def contact_page(request):
+    return render(request, 'pages/contact-us.html')
+
+def about_page(request):
+    return render(request, 'pages/about-us.html')
