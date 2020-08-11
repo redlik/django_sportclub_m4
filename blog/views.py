c967fa85d4b2ffaee6ddb9e64a7ed5ef7aa6b560
Ralph - Collage Creative <ralphr@outlook.com>
2020-08-11 08:22:55 +0100

    Changed news template folder from 'blog' for consistency

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,11 @@ from .models import Post
 
 def posts_list(request):
     posts = Post.published.all()
-    
-    return render(request, 'blog/post/list_of_posts.html', {'posts': posts})
+
+    return render(request, 'news/list_of_posts.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish_year=year, publish_month=month, publish_day=day)
 
-    return render(request, 'blog/post/detail_view.html', {'post': post})
+    return render(request, 'news/detail_view.html', {'post': post})
 
