059a6caf031230a69e0e78a0ce4ee0f4bb4b4d5c
Ralph - Collage Creative <ralphr@outlook.com>
2020-08-11 20:51:48 +0100

    Changed Category friendly name to mandatory

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=120)
-    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+    friendly_name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
@@ -11,12 +11,12 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
-    class Product(models.Model):
-        name = models.CharField(max_length=254)
-        description = models.TextField()
-        image = models.ImageField(upload_to='images/products/', blank=True)
-        category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-        price = models.DecimalField(max_digits=6, decimal_places=2)
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/products/', blank=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
