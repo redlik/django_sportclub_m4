e3e59a7457a81d0ee129dd94e2581c385404c331
Ralph - Collage Creative <ralphr@outlook.com>
2020-08-11 20:35:18 +0100

    Products and Category models created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=120)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+    class Product(models.Model):
+        name = models.CharField(max_length=254)
+        description = models.TextField()
+        image = models.ImageField(upload_to='images/products/', blank=True)
+        category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+        price = models.DecimalField(max_digits=6, decimal_places=2)
+
+        def __str__(self):
+            return self.name
