c39e63dec8c6f228c879ae29e271bcab735ce91b
Ralph - Collage Creative <ralphr@outlook.com>
2020-08-11 08:21:44 +0100

    News url included into main urls file

--- united/urls.py
+++ united/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('news/', include('blog.urls', namespace='news')),
     path('', include('homepage.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
