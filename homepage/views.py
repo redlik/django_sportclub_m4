b029762dd3a16f03f8a161734b297c08ca9625bd
redlik <ralphr@outlook.com>
2020-08-11 14:21:45 +0100

    Homepage list all posts, first 3 as headings

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def index(request):
     """ View to display homepage """
-    return render(request, 'homepage/index.html')
+    headings = Post.published.all()[:3]
+    posts = Post.published.all()[3:99]
+    return render(request, 'homepage/index.html', {'headings':headings, 'posts': posts})
