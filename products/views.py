051d73982d5803ae8d8eb82cef6c993a00f2ad00
redlik <ralphr@outlook.com>
2020-08-12 16:27:26 +0100

    Category filtering added to views file

--- products/views.py
+++ products/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from .models import Product, Category
 
 def all_products(request):
     products = Product.objects.all()
+    categories = Category.objects.all()
+    current_category = None
 
-    return render(request, 'products/all_products.html', {'products': products})
+    if request.GET:
+        if 'category' in request.GET:
+            category = request.GET['category']
+            products = products.filter(category__name=category)
+            current_category = categories.filter(name__icontains=category).first()
+
+    return render(request, 'products/all_products.html', {'products': products, 'categories': categories, 'current_category': current_category,})
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
