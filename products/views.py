8d0a519500dee52b053f1fa61e5a800feac1fb25
Ralph - Collage Creative <ralphr@outlook.com>
2020-08-12 08:35:13 +0100

    Views for products created, both list and detailed

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+def all_products(request):
+    products = Product.objects.all()
+
+    return render(request, 'products/all_products.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    return render(request, 'products/product_detail.html', {'product': product})
+
+
+
