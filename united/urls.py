a6991beaa96a9fdd60e8d551585e075d00bcf96f
redlik <ralphr@outlook.com>
2020-08-12 17:49:43 +0100

    Added Basket urls to main urls file

--- united/urls.py
+++ united/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('homepage.urls')),
     path('', include('pages.urls')),
     path('', include('products.urls')),
+    path('basket/', include('basket.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
