fb71c05c965f007a22f52b28bfdf531f41d870d1
redlik <ralphr@outlook.com>
2020-12-07 07:17:46 +0000

    Heading to shop page included

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Product, Category
 
+
 def all_products(request):
     products = Product.objects.all()
     categories = Category.objects.all()
@@ -12,12 +13,11 @@ def all_products(request):
             products = products.filter(category__name=category)
             current_category = categories.filter(name__icontains=category).first()
 
-    return render(request, 'products/all_products.html', {'products': products, 'categories': categories, 'current_category': current_category,})
+    return render(request, 'products/all_products.html',
+                  {'products': products, 'categories': categories, 'current_category': current_category, })
+
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
     return render(request, 'products/product_detail.html', {'product': product})
-
-
-
