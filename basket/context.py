7a94f95124bb0485c7f0ca9e38320b210b1e2e2f
Ralph - Collage Creative <ralphr@outlook.com>
2020-12-13 18:38:30 +0000

    basket session object added to context file

--- basket/context.py
+++ basket/context.py
@@ -1,9 +1,25 @@
+from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 def basket_contents(request):
 
     basket_items = []
     total = 0
     product_count = 0
-    grand_total = 0
+    basket = request.session.get('basket', {})
+
+    for product_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=product_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'product_id': product_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    grand_total = total
 
     context = {
         'basket_items': basket_items,
