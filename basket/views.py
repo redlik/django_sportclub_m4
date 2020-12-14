f5b7f26e90e5f6f818a23eb820097ee3c6f1d0ef
redlik <ralphr@outlook.com>
2020-12-14 11:58:10 +0000

    Changed add_to_basket function to include items with sizes

--- basket/views.py
+++ basket/views.py
@@ -4,18 +4,33 @@ from django.shortcuts import render, redirect
 def view_basket(request):
     return render(request, 'basket/basket.html')
 
+
 def add_to_basket(request, product_id):
-    """ Function to add product and quantity to the basket """
+    """ Function to add product,size and quantity to the basket """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     basket = request.session.get('basket', {})
 
-    if product_id in list(basket.keys()):
-        basket[product_id] += quantity
+    if size:
+        if product_id in list(basket.keys()):
+            if size in basket[product_id]['products_by_size'].keys():
+                basket[product_id]['products_by_size'][size] += quantity
+            else:
+                basket[product_id]['products_by_size'][size] = quantity
+        else:
+            basket[product_id] = {'products_by_size': {size: quantity}}
     else:
-        basket[product_id] = quantity
+        if product_id in list(basket.keys()):
+            basket[product_id] += quantity
+        else:
+            basket[product_id] = quantity
 
     request.session['basket'] = basket
+
+    print(basket)
 
     return redirect(redirect_url)
