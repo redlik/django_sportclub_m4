a4189a334b3a11dcd15344f0915f970ffb95f2f8
Ralph - Collage Creative <ralphr@outlook.com>
2020-12-18 07:18:10 +0000

    Admin information for Order model included

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date', 'delivery_cost', 'order_total', 'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode', 'city', 'address1', 'address2', 'delivery_cost', 'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name', 'ordet_total', 'delivery_cost', 'grand_total'),
+
+    ordering = ('-date',)
