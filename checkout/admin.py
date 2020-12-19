12abeafb0806648572a2360274aae622a361e95a
redlik <ralphr@outlook.com>
2020-12-19 09:51:29 +0000

    Order details panel added to Admin dashboard

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
 class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
     readonly_fields = ('order_number', 'date', 'delivery_cost', 'order_total', 'grand_total',)
 
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode', 'city', 'address1', 'address2', 'delivery_cost', 'order_total', 'grand_total',)
+    fields = (
+        'order_number', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode', 'city', 'address1',
+        'address2', 'delivery_cost', 'order_total', 'grand_total',)
 
-    list_display = ('order_number', 'date', 'full_name', 'ordet_total', 'delivery_cost', 'grand_total'),
+    list_display = ('order_number', 'date', 'full_name', 'order_total', 'delivery_cost', 'grand_total',)
 
     ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
