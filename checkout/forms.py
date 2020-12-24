7ecd042e34f286c38dd463858fedbe58fd72462e
redlik <ralphr@outlook.com>
2020-12-24 07:36:49 +0000

    Order form created

---
+++ checkout/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number', 'address1', 'address2', 'city', 'postcode', 'country')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email',
+            'phone_number': 'Phone Number',
+            'address1': 'Address Line 1',
+            'address2': 'Address Line 2',
+            'city': 'City/Town',
+            'postcode': 'Postcode',
+            'country': 'Country',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = f'{placeholders[field]}'
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
