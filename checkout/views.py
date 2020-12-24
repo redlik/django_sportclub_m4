e0da076fa149376a3003769a3d60dc8fae671f7c
redlik <ralphr@outlook.com>
2020-12-24 10:46:18 +0000

    Order form view created

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "The basket is empty at the moment")
+        return redirect(reverse('shop:all_products'))
+
+    order_form = OrderForm()
+    context = {
+        'order_form': order_form
+    }
+
+    return render(request, 'checkout/checkout.html', context)
