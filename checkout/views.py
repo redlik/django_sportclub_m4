d01bdaa63aea64c967afd04604fd64109dc97710
Ralph - Collage Creative <ralphr@outlook.com>
2020-12-25 17:32:28 +0000

    Stripe form Javascript complete, payment appears on Stripe Dashboard

--- checkout/views.py
+++ checkout/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
 
+import stripe
+
 from .forms import OrderForm
+from basket.context import basket_contents
 
 
 def checkout(request):
@@ -14,6 +17,17 @@ def checkout(request):
         messages.error(request, "The basket is empty at the moment")
         return redirect(reverse('shop:all_products'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount = stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
 
     if not stripe_public_key:
@@ -22,8 +36,8 @@ def checkout(request):
 
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_4DsMDDIsp4VTuDwC1AfU1mmo',
-        'client_secret': 'client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
