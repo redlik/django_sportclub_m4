3ef6181fb5e3a2128bd5f6d594752330f88716b2
redlik <ralphr@outlook.com>
2020-12-25 13:38:45 +0000

    Stripe variables added to app settings

--- checkout/views.py
+++ checkout/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "The basket is empty at the moment")
         return redirect(reverse('shop:all_products'))
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
+
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_4DsMDDIsp4VTuDwC1AfU1mmo',
+        'client_secret': 'client secret',
     }
 
     return render(request, 'checkout/checkout.html', context)
--- united/settings.py
+++ united/settings.py
@@ -187,3 +187,8 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
