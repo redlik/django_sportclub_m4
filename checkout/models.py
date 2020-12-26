aacfa51624df1c59cccf728542c06cca9fcb5920
redlik <ralphr@outlook.com>
2020-12-26 16:59:50 +0000

    Fixed Order model file, totals were missing default value

--- checkout/models.py
+++ checkout/models.py
@@ -18,29 +18,33 @@ class Order(models.Model):
     address1 = models.CharField(max_length=32, null=False, blank=False)
     address2 = models.CharField(max_length=32, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    order_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
-    grand_total = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
         """
-        Generate uniquem random order number
+        Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
 
-    def save(self, *args, **kwargs):
-        if not self.order_number:
-            self.order_number = self._generate_order_number
-        super().save(*args, **kwargs)
-
     def update_total(self):
-        self.order_total = self.lineitem.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitem.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < 100:
             self.delivery_cost = 5
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.order_number
