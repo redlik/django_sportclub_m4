16f46d3fa6a66136afc7b3b9b150dc535314e20f
redlik <ralphr@outlook.com>
2020-12-26 16:59:06 +0000

    Checkout success function added to views file

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
@@ -30,9 +30,10 @@ def checkout(request):
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save()
-            for item_id, item_data in basket.items():
+            print(order)
+            for product_id, item_data in basket.items():
                 try:
-                    product = Product.objects.get(id=item_id)
+                    product = Product.objects.get(id=product_id)
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
                             order=order,
@@ -41,7 +42,7 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for size, quantity in item_data['products_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
@@ -64,6 +65,7 @@ def checkout(request):
                     Please double check your information.')
     else:
         basket = request.session.get('basket', {})
+        print(basket)
         if not basket:
             messages.error(request, "The basket is empty at the moment")
             return redirect(reverse('shop:all_products'))
@@ -73,11 +75,9 @@ def checkout(request):
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
-            amount = stripe_total,
+            amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-
-        print(intent)
 
         order_form = OrderForm()
 
@@ -92,3 +92,24 @@ def checkout(request):
         }
 
         return render(request, 'checkout/checkout.html', context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'basket' in request.session:
+        del request.session['basket']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
