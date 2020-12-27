921f07744f89d19b886a05941ad854b74498a0a3
redlik <ralphr@outlook.com>
2020-12-27 08:38:45 +0000

    Added query to pick 3 random products in homepage views file

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from blog.models import Post
+from products.models import Product
 
 def index(request):
     """ View to display homepage """
     headings = Post.published.all()[:3]
     posts = Post.published.all()[3:99]
-    return render(request, 'homepage/index.html', {'headings':headings, 'posts': posts})
+    featured_products = Product.objects.order_by('?')[:3]
+    context = {
+        'headings': headings,
+        'posts': posts,
+        'featured_products': featured_products,
+    }
+
+    return render(request, 'homepage/index.html', context)
