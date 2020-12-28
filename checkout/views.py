83e5ec0d8c10418272ee113c03800d1b92a61ea5
redlik <ralphr@outlook.com>
2020-12-28 20:42:17 +0000

    Fixed field name in checkout view 'town_or_city' to 'city'

--- checkout/views.py
+++ checkout/views.py
@@ -149,9 +149,9 @@ def checkout_success(request, order_number):
                 'default_phone_number': order.phone_number,
                 'default_country': order.country,
                 'default_postcode': order.postcode,
-                'default_city': order.town_or_city,
-                'default_address1': order.street_address1,
-                'default_address2': order.street_address2,
+                'default_city': order.city,
+                'default_address1': order.address1,
+                'default_address2': order.address2,
             }
             user_profile_form = UserProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
