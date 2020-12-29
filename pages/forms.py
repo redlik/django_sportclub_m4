99c4a861caa1623663d242cbea8bba00c432497e
Ralph - Collage Creative <ralphr@outlook.com>
2020-12-29 21:25:36 +0000

    Membership form styling and extra images added

--- pages/forms.py
+++ pages/forms.py
@@ -15,5 +15,6 @@ class ContactForm(forms.ModelForm):
 class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
-        fields = "__all__"
+        fields = ['first_name', 'last_name', 'date_of_birth', 'email', 'phone_number', 'address1', 'address2', 'city',
+                  'postcode', 'country',]
 
